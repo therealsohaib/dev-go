f725ef6c20c25578805944a6e6c7eda8a582b051
Sohaib Hassan <sohaib@elgrocer.com>
2025-09-02 20:40:07 +0500

    Associations added in the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :books , through: :borrows, dependent: authors
+
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }
